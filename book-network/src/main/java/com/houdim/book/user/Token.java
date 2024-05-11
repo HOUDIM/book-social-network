af37ed5833fa048097cef6f6da0b9fd084fca2f1
Houssem DIMASSI <77940811+HOUDIM@users.noreply.github.com>
2024-05-11 23:36:15 +0100

    first commi

---
+++ book-network/src/main/java/com/houdim/book/user/Token.java
@@ -0,0 +1,27 @@
+package com.houdim.book.user;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+import java.time.LocalDateTime;
+
+@Getter
+@Setter
+@Builder
+@AllArgsConstructor
+@NoArgsConstructor
+@Entity
+public class Token {
+
+    @Id
+    @GeneratedValue
+    private Integer id;
+    private String token;
+    private LocalDateTime createdAt;
+    private LocalDateTime expiresAt;
+    private LocalDateTime validatedAT;
+
+    @ManyToOne
+    @JoinColumn(name = "userId" , nullable = false)
+    private User user;
+}
---
+++ book-network/src/main/java/com/houdim/book/user/TokenRepository.java
@@ -0,0 +1,12 @@
+package com.houdim.book.user;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import java.util.Optional;
+
+public interface TokenRepository extends JpaRepository<Token , Integer> {
+
+    Optional<Token> findByToken(String token);
+
+
+}
